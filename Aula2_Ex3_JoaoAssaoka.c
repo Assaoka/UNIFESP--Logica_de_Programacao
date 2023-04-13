9cb843515b205eb5886f782419469aaca7f2d0bc
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-04-13 14:10:22 -0300

    Add files via upload

---
+++ Aula2_Ex3_JoaoAssaoka.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+  int N;
+  
+  printf("Digite um Valor Inteiro: ");
+  scanf(" %d",&N);
+  printf("--------------------------\n");
+  printf("%d^2 = %d\n",N,N*N);
+  printf("%d^3 = %d\n",N,N*N*N);
+  printf("√%d = %f\n",N,sqrt(N));    
+  printf("--------------------------\n");
+  
+  return 0;
+}
