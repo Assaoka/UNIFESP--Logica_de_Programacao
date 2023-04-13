d6a35754902d194de50fa81d5d2d147de270fc1d
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-04-13 01:30:15 -0300

    Update Aula2_Ex1_JoaoAssaoka.c

--- Aula2_Ex1_JoaoAssaoka.c
+++ Aula2_Ex1_JoaoAssaoka.c
@@ -9,5 +9,7 @@ int main(void) {
   printf("Quantos dólares você tem? $ ");
     scanf("%f",&N_Dolar);
 
-  printf("Você tem $ %.2f, considerando a cotação atual como %.2f, você tem R$ %.2f!",N_Dolar,Cot_Dolar,N_Dolar*Cot_Dolar);    
+  printf("Você tem $ %.2f, considerando a cotação atual como %.2f, você tem R$ %.2f!",N_Dolar,Cot_Dolar,N_Dolar*Cot_Dolar);
+
+  return 0;
 }
