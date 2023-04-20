adce751fc38dcaf02d6583615bd98a4e20f78ff0
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-04-20 00:24:01 -0300

    Update Aula2_Ex3_JoaoAssaoka.c

--- Aula2_Ex3_JoaoAssaoka.c
+++ Aula2_Ex3_JoaoAssaoka.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
 
 int main(void) {
   int N;
   
   printf("Digite um Valor Inteiro: ");
   scanf(" %d",&N);
+  
   printf("--------------------------\n");
   printf("%d^2 = %d\n",N,N*N);
   printf("%d^3 = %d\n",N,N*N*N);
