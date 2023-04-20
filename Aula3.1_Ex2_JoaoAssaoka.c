db8f9d5080ed59b91e2ebc7694487caa78eebfa8
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-04-20 01:09:30 -0300

    Add files via upload

---
+++ Aula3.1_Ex2_JoaoAssaoka.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+float A, B, C;
+  scanf(" %f %f %f", &A, &B, &C);
+  printf("TRIANGULO: %.3f\nCIRCULO: %.3f\nTRAPEZIO: %.3f\nQUADRADO: %.3f\nRETANGULO: %.3f\n",A*C/2,3.14159*C*C,(A+B)*C/2,B*B,A*B);
+  return 0;
+}
