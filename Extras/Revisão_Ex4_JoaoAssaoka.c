7adc9ac263695ebeb6ff2c4934dde036679ebd14
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-07-20 10:19:32 -0300

    Update Revisão_Ex4_JoaoAssaoka.c

--- Extras/Revisão_Ex4_JoaoAssaoka.c
+++ Extras/Revisão_Ex4_JoaoAssaoka.c
@@ -2,20 +2,29 @@
 
 #include <stdio.h>
 
-void maior_menor (float vetor[], float *menor, float *maior) {
-	*menor = *maior = vetor[0];
+void maior_menor (float vetor[], float *menor, float *maior, int *dia_menor, int *dia_maior) {
+	// Como temos que retornar 4 valores, estamos usando ponteiros. O operador "vai para" * associado com o endereço que recebemos da main atualiza nossa variável lá na main.
+	*menor = *maior = vetor[0]; // Usar um valor fixo pode ocasionar em erros, então iniciamos com a primeira casa do vetor
+	*dia_maior = *dia_menor = 0; // Pelo que fizemos em cima, o dia deve começar em 0. 
 	
-	int i = 1;
+	int i = 1; // Como já escolhemos o valor da posição 0 como inicial, podemos começar na casa 1
 	do {
-		if (*menor > vetor[i])
+		if (*menor > vetor[i]) {
+			// Se o menor valor que achamos até o momento for menor que o que estamos analizando, mudamos os valores que representam o menor
 			*menor = vetor[i];
-		if (*maior < vetor[i])
+			*dia_menor = i; // Poderia passar apenas essa informação e usar vetor[dia_menor] na main, coloquei assim para facilitar a leitura 
+		}
+		if (*maior < vetor[i]) {
+			// Se o maior valor for menor que o que estamos analizando...
 			*maior = vetor[i];
-	} while (++i < 7);
+			*dia_maior = i;
+		}
+	} while (++i < 7); // Repete 6 vezes (seria 7 nessa estrutura se i = 0), mas começamos no 1
 }
 
 int main () {
 	float vetor[7], montante, menor, maior;
+	int dia_menor, dia_maior;
 
 	printf("Quantos dólares vc tem? U$ "), scanf(" %f", &montante);
 
@@ -25,10 +34,11 @@ int main () {
 		printf("Cotação dia %d: ", i), scanf(" %f", &vetor[i]);
 	} while (++i < 7);
 
-	maior_menor(vetor, &menor, &maior);
+	// & = Endereço, usando isso porquê precisamos retornar 4 valores
+	maior_menor(vetor, &menor, &maior, &dia_menor, &dia_maior);
 
-	printf("\nMontante (maior cotação): R$ %.2f", montante*maior);
-	printf("\nMontante (menor cotação): R$ %.2f", montante*menor);
+	printf("\nMontante no dia %d (maior cotação): R$ %.2f", dia_maior, montante*maior);
+	printf("\nMontante no dia %d (menor cotação): R$ %.2f", dia_menor, montante*menor);
 		
 	return 0;
 }
