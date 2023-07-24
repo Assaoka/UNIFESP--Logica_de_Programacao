285c6d875c7f4a6a207c60e69028280ff4900889
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-07-23 23:21:45 -0300

    Create Aula009_Ex2_JoaoAssaoka.c

---
+++ Exercícios P2/Aula009_Ex2_JoaoAssaoka.c
@@ -0,0 +1,16 @@
+// 2-) Faça um programa para resolver o seguinte problema: Dados um número inteiro n > 0 e uma sequência com n números inteiros, determinar o menor e o maior número da sequência. Por exemplo, para n=6 e para a sequência com 6 números inteiros = {6 −27 0 −5 84 -33} o seu programa deve escrever o número -33.
+
+#include <stdio.h>
+
+int main(void) {
+  int V[100], N, i = 0, menor = 777777;
+
+  scanf(" %d", &N);
+  do {
+    scanf(" %d", &V[i]);
+    if (menor > V[i])
+      menor = V[i];
+    printf("N = %d, i = %d, menor = %d\n", N, i , menor);
+  } while (++i<N);
+  return 0;
+}
