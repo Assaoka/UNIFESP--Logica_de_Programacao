40f7ab792aec9f85959d90c636f18e8b07af995a
João Assaoka <joaovictorar11+GitHub@gmail.com>
2024-02-13 00:41:21 -0300

    [2] Operadores

---
+++ [ 2] Operadores/Aula2_Ex3_JoaoAssaoka.c
@@ -0,0 +1,15 @@
+// Aula 2 - Exercício 3 - João Assaoka
+// Calcule a média ponderada segundo as regras do enunciado.
+
+#include <stdio.h>
+
+int main() {
+    float P1, P2, T1, T2, T3, M1, M2, M3;
+    
+    scanf(" %f %f %f %f %f %f", &P1, &P2, &T1, &T2, &T3, &M3);
+    M1 = (P1+P2)/2;
+    M2 = (T1+T2+T3)/3;
+    
+    printf("M1 = %f\nM2 = %f\nM3 = %f\n-----------------\nNota Final = %f", M1, M2, M3, (6*M1+3*M2+M3)/10);
+    return 0;
+}
