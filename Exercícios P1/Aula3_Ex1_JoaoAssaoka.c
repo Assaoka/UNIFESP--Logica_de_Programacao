4d4068e26f733c4fa6fca63d37a438252e422009
João Assaoka <joaovictorar11+GitHub@gmail.com>
2024-02-12 23:54:34 -0300

    Reorganização 1

--- Exercícios P1/Aula3_Ex1_JoaoAssaoka.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-double A, B, C;
-scanf(" %lf %lf %lf",&A,&B,&C);
-printf("MEDIA = %.1lf\n",(2*A+3*B+5*C)/10);
-return 0;
-}
--- Exercícios P1/Aula3_Ex2_JoaoAssaoka.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-  int N, Ht;
-  float VpH;
-  scanf(" %d %d %f",&N,&Ht,&VpH);
-  printf("NUMBER = %d\nSALARY = U$ %.2f\n",N,Ht*VpH);
-  return 0;
-}
